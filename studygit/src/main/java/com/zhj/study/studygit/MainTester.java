33f78284f3d994a4d99164a9b91f615acdf31bf2
shinsanzhj <mindofzhj@163.com>
2016-11-14 14:30:25 +0800

    我在eclipse创建了一个项目

---
+++ studygit/src/main/java/com/zhj/study/studygit/MainTester.java
@@ -0,0 +1,17 @@
+package com.zhj.study.studygit;
+
+public class MainTester {
+
+	/**
+	 * @param args
+	 *
+	 * <pre>
+	 * 修改日期		修改人	修改原因
+	 * 2016-11-14	钟华杰	新建
+	 * </pre>
+	 */
+	public static void main(String[] args) {
+		System.out.println("这个项目是用来学习git的");
+	}
+
+}
